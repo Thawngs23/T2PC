3bab4295a136aa00de5fd340990e852cb689cec4
unknown <quangtientls@gmail.com>
2021-04-10 22:49:33 +0700

    chay test handlebars ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const path = require('path')
+const exphbs = require('express-handlebars')
+const { extname, join } = require('path')
 
+app.engine('hdb', exphbs({extname: '.hdb'}))
+app.set('view engine', 'hdb')
+
+app.set('views',path.join(__dirname,'resources/views'))
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!')
+  res.render('home')
 })
 
 app.listen(port, () => {
