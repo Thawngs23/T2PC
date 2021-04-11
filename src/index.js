ea8b93348844e03049dd2cf25829770f2fd0dcb9
unknown <quangtientls@gmail.com>
2021-04-11 17:44:19 +0700

    them thanh header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const path = require('path')
+const 
+path = require('path')
 const exphbs = require('express-handlebars')
 const { extname, join } = require('path')
 
-app.engine('hdb', exphbs({extname: '.hdb'}))
-app.set('view engine', 'hdb')
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 app.set('views',path.join(__dirname,'resources/views'))
 app.get('/', (req, res) => {
