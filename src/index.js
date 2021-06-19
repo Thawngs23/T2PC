1426ee8664886f17427fccfc2f0cca4640a05d39
QTienTLS <quangtientls@gmail.com>
2021-06-19 21:37:27 +0700

    route + controller

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const 
-path = require('path')
+const
+  path = require('path')
 const exphbs = require('express-handlebars')
 const { extname, join } = require('path')
+const route = require('./routes/index.route')
 
+route(app)
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
-app.set('views',path.join(__dirname,'resources/views'))
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.set('views', path.join(__dirname, 'resources/views'))
+
 //sử dụng file tĩnh
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ src/routes/index.route.js
@@ -0,0 +1,11 @@
+const newsRouter = require('./news.route');
+
+function route(app) {
+
+    app.get('/', (req, res) => {
+        res.render('home');
+    })
+    app.use('/news',newsRouter);
+}
+
+module.exports = route;
