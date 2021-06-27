44f8a2ab3757b5c85d49bc283818e4528f234661
QTienTLS <quangtientls@gmail.com>
2021-06-27 17:59:18 +0700

    chức năng upload ảnh + thêm event

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const db = require('./config/db');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(methodOverride('_method'));
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../app/controllers/AdminController');
 
-router.get('/dashboard',adminController.dashboard);
-router.get('/event',adminController.event);
+router.get('/dashboard', adminController.dashboard);
+router.get('/event', adminController.event);
+router.post('/event/add-event', adminController.addEvent);
 
 module.exports = router;
